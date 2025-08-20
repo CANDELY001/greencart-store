9b3234c51b2f82e79eac7bc2aa248be05b2d7428
CandelyStack <candely1charifa@gmail.com>
2025-08-20 20:18:31 +0100

    add user routes & register method & update DB connection

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,6 +21,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => {
   res.send("API is working");
 });
+app.use("/api/user", userRouter);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
