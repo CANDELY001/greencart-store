f132797411104010b4ef0dd98e8ffaba98018959
CandelyStack <candely1charifa@gmail.com>
2025-08-20 23:29:19 +0100

    Products Controllers

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,11 +1,70 @@
+import { v2 as cloudinary } from "cloudinary";
+import Product from "server/models/productModel.js";
+
 // Add product : /api/product/add
-export const addProduct = async (req, res) => {};
+export const addProduct = async (req, res) => {
+  try {
+    let productData = JSON.parse(req.body.productData);
+    const images = req.files;
+    let imagesURL = await Promise.all(
+      images.map(async (image) => {
+        let result = await cloudinary.uploader.upload(image.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+    await Product.create({ ...productData, images: imagesURL });
+    res.json({ sucess: true, message: "Product added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Get all products : /api/product/list
-export const productList = async (req, res) => {};
+export const productList = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Get single product : /api/product/id
-export const productById = async (req, res) => {};
+export const productById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //change product instock : /api/product/stock
-export const changeStock = async (req, res) => {};
+export const changeStock = async (req, res) => {
+  try {
+    const { id, inStock } = req.body;
+    await Product.findByIdAndUpdate(id, { inStock });
+    res.json({ success: true, message: "Product stock updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
