227826343afdceef0a6d94e457ce34cfb5e034da
CandelyStack <candely1charifa@gmail.com>
2025-08-20 19:19:10 +0100

    Successfully Connected to mongoDB Database

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./configs/db.js";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+await connectDB();
+
 //Allow multiple origins
-const allowedOrgins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173"];
 
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrgins, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("API is working");
