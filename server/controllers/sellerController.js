448e2a39cc7b0ca171aed174a96e1fe4976374c9
CandelyStack <candely1charifa@gmail.com>
2025-08-20 22:37:56 +0100

    Seller Controller

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,53 @@
+//Login seller: /api/seller/login
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      password === process.env.SELLER_PASSWORD &&
+      email === process.env.SELLER_EMAIL
+    ) {
+      const token = jwt.sign({ email }, process.env.SELLER_JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.cookies("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      });
+      return res.json({ sucess: true, message: "Login successful" });
+    } else {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+//Check Auth /api/seller/is-auth
+export const isAuth = async (req, res) => {
+  try {
+    return res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Logout User api/seller/logout
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({ success: true, message: "Logged out" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
